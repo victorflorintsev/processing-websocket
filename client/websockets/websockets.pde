0b18e02ec8b8a93ec8fec3502f72eacf47367eb2
victorflorintsev <victorflorintsev1996@gmail.com>
2018-03-15 19:18:51 -0500

    First steps
    got localhost server to accept the positions clicked of boxes added by clients.

---
+++ client/websockets/websockets.pde
@@ -0,0 +1,36 @@
+import websockets.*;
+
+WebsocketClient ws;
+
+
+class Player {
+  PVector pos;
+  
+  Player(int x, int y) {
+    pos = new PVector(x,y);
+    noStroke();
+    fill(255, 77, 77);
+    rect(x,y,20,20);
+  }
+  
+  String toJSON() {
+   return "{'event': 'newPlayer', 'x': " + pos.x + ", 'y': " + pos.y + "}"; 
+  }
+  
+}
+
+ArrayList<Player> list = new ArrayList<Player>();
+
+void setup() {
+  size(1920,1080);
+  background(75);
+  ws = new WebsocketClient(this, "ws://localhost:3000/");
+}
+
+void draw() {
+ if (mousePressed) {
+  Player newPlayer = new Player(mouseX, mouseY);
+  list.add(newPlayer);
+  ws.sendMessage(newPlayer.toJSON());
+ }
+}
---
+++ server.js
@@ -0,0 +1,41 @@
+
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+console.log("My socket server is running");
+
+var websocket = require('websocket').server;
+
+wsServer = new websocket({
+  httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+  var connection = request.accept(null, request.origin);
+  console.log("We got someone on");
+  // This is the most important callback for us, we'll handle
+  // all messages from users here.
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+    	console.log("We got an event: " + message.utf8Data);
+    	try {
+    		var command = JSON.parse(message.utf8Data);
+	    } catch (e) {
+	    	console.log("couldn't parse data.");
+	    	throw e;
+	    	return;
+	    }
+       if (command.event == "newPlayer") {
+       	
+       	console.log("NewPlayer at coordinates: x = " + command.x + " y = " + command.y);
+       }
+    }
+  });
+
+  connection.on('close', function(connection) {
+    // close user connection
+  });
+});
